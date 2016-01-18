a555f3a87cb4ef8816abae47119e1c14106a0b6f
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-01-18 11:06:18 -0800

    Setup RetiredForagerBee to inherit from ForagerBee while pointing constructor to ForagerBee

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,6 @@
 var RetiredForagerBee = function() {
+	ForagerBee.call(this);
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.constructor = RetiredForagerBee;
